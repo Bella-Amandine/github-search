eb724ad0a1f845831f7db8e2a58482572e6faf03
Bella Amandine <bellamandy45@gmail.com>
2020-11-09 11:40:22 +0200

    Add a pipe that remove the dash to the reposioty name

--- src/app/split-dash.pipe.ts
+++ src/app/split-dash.pipe.ts
@@ -5,8 +5,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SplitDashPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string): string {
+    let newString = value.replace("-", " ");
+
+     return newString;
   }
 
 }
